b5c0f26bac9f8d006d465c6d0ee9161e7a14242c
sputgarin <89846079+sputgarin@users.noreply.github.com>
2022-11-22 10:07:05 +0100

    Created the gesture script and got the gestures are working

---
+++ Assets/Witchery/Scripts/AddAmmoTest.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddAmmoTest : MonoBehaviour
+{
+    public int ammo = 0;
+
+
+    public void addAmmoWhenXPattern(string patternName)
+    {
+        if (patternName == "X")
+        {
+            Debug.Log("X was Drawn");
+        }
+
+        else if (patternName == "P")
+        {
+            Debug.Log("P was Drawn");
+        }
+    }
+}
